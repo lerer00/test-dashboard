2f06c738216423b78f4ca7a7362a5d78b14cb294
Francis Boily <boilyfrank28@gmail.com>
2022-08-18 13:45:53 -0400

    trying stuff with env variable

--- lib/useFirebaseAuth.jsx
+++ lib/useFirebaseAuth.jsx
@@ -11,7 +11,6 @@ export default function useFirebaseAuth() {
     const [loading, setLoading] = useState(true);
 
     const authStateChanged = async (authState) => {
-        console.log(authState)
         if (!authState) {
             setLoading(false)
             return;
